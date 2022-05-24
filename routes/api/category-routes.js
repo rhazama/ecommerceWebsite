50c7319b6b78a17e593329c2b9863f6d536c4733
rhazama <rhazama01@gmail.com>
2022-05-23 18:40:23 -0700

    added tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
       attributes:['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-  .then(dbPostData => res.json(dbPostData))
+  .then(dpPostData => res.json(dpPostData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
       attributes:['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-  .then(dbPostData => res.json(dbPostData))
+  .then(dpPostData => res.json(dpPostData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
